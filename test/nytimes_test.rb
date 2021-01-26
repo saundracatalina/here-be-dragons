b15e3131f88ae61a8a78835f14e85c4618437f85
Saundra <saundracatalina@gmail.com>
2021-01-26 16:49:42 -0700

    All tests passing

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -12,12 +12,14 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_copyright
     #Set your code to the local variable, "result"
+    result = @hash[:copyright]
 
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
 
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
+    result = @hash[:results]
 
     assert result.is_a? (Array)
     assert_equal 44, result.count
@@ -25,8 +27,12 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_all_stories_with_subsection_of_politics
     #Set your code to the local variable, "result"
-
-
+    result = []
+    @hash[:results].each do |res|
+      if res[:subsection] == "Politics"
+        result << res
+      end
+    end
     assert result.is_a? (Array)
     assert_equal 6, result.count
     assert_equal "Congressional G.O.P. Agenda Quietly Falls Into Place Even as Trump Steals the Spotlight",
@@ -34,5 +40,4 @@ class NytimesTest < Minitest::Test
     assert_equal "Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down.",
       result.last[:title]
   end
-
 end
